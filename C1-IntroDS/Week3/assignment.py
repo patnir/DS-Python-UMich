59c24ea2ca63c10539851fb119cc2b184ab2ddde
Rahul Patni <rpatni@purdue.edu>
2018-05-31 19:55:05 -0400

    assignment 2 & 3 upload

--- C1-IntroDS/Week3/assignment.py
+++ C1-IntroDS/Week3/assignment.py
@@ -57,3 +57,72 @@ newdf2 = newdf2[["Country", 'Rank', 'Documents', 'Citable documents', 'Citations
 newdf2 = newdf2.set_index("Country").sort_values(by=['Rank'], ascending=True)
 print(newdf2)
 
+
+def answer_one():
+    energy = get_energy_df()
+    GDP = get_GDP_df()
+    ScimEn = get_ScimEn_df()
+    newdf = pd.merge(energy, ScimEn, how="inner", left_on='Country', right_on='Country')
+
+    newdf2 = pd.merge(newdf, GDP, how="inner", left_on='Country', right_on='Country')
+
+    newdf2 = newdf2[["Country", 'Rank', 'Documents', 'Citable documents', 'Citations', 'Self-citations', 'Citations per document', 'H index', 'Energy Supply', 'Energy Supply per Capita', '% Renewable', '2006', '2007', '2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015']]
+    newdf2 = newdf2.set_index("Country").sort_values(by=['Rank'], ascending=True)
+
+    return newdf2, energy, GDP, ScimEn
+
+
+def answer_three():
+    Top15 = answer_one()[0]
+    rows=['2006', '2007', '2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015']
+    Top15["avgGDP"] = Top15.apply(lambda x: np.mean(x[rows]), axis=1)
+    Top15 = Top15.sort_values(by=['avgGDP'], ascending=False)
+    return Top15["avgGDP"].rename('avgGDP')
+
+
+def answer_two():
+    union = pd.merge(pd.merge(energy, GDP, on='Country', how='outer'), ScimEn, on='Country', how='outer')
+    intersect = pd.merge(pd.merge(energy, GDP, on='Country'), ScimEn, on='Country')
+    return len(union)-len(intersect)
+
+
+def answer_four():
+    allCountries = answer_one()[0]
+    Top15 = answer_three()
+    country = Top15.index.tolist()[5]
+    res = allCountries.loc[country]["2015"] - allCountries.loc[country]["2006"]
+    return res
+
+
+def answer_five():
+    Top15 = answer_one()[0]
+
+    return np.mean(Top15["Energy Supply per Capita"])
+
+
+def answer_six():
+    Top15 = answer_one()[0]
+    mpr = np.max(Top15["% Renewable"])
+    return Top15["% Renewable"].idxmax(), mpr
+
+
+def answer_seven():
+    Top15 = answer_one()[0]
+    Top15["CitRatio"] = Top15["Self-citations"] / Top15["Citations"]
+    mpr = np.max(Top15["CitRatio"])
+    return Top15["CitRatio"].idxmax(), mpr
+    return Top15
+
+
+answer_seven()
+
+answer_six()
+
+answer_five()
+
+answer_four()
+
+answer_two()
+
+answer_three()
+
