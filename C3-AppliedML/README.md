b0c9b812420efd03c0198e4cc8dff62e5a2f2dd8
Rahul Patni <rpatni@purdue.edu>
2018-06-11 17:17:02 -0400

    lecture slides for course 3

--- C3-AppliedML/README.md
+++ C3-AppliedML/README.md
@@ -2,5 +2,6 @@
 
 [The Foundations of Algorithmic Bias](http://approximatelycorrect.com/2016/11/07/the-foundations-of-algorithmic-bias/)
 [Data Science Ethics Course](https://www.edx.org/course/data-science-ethics)
-
+[A Few Useful Things to Know about Machine Learning](https://homes.cs.washington.edu/~pedrod/papers/cacm12.pdf)
+[Genetic Test for Autism Refuted](https://www.the-scientist.com/?articles.view/articleNo/38030/title/Genetic-Test-for-Autism-Refuted/)
 
--- C3-AppliedML/Week2/test-1.py
+++ C3-AppliedML/Week2/test-1.py
@@ -10,10 +10,37 @@ np.set_printoptions(precision=2)
 
 fruits = pd.read_table("../Week1/fruit_data_with_colors.txt")
 
-feature_names_fruits = ["height", "width", "mass", "color_space"]
+feature_names_fruits = ["height", "width", "mass", "color_score"]
 X_fruits = fruits[feature_names_fruits]
 y_fruits = fruits['fruit_label']
 
 
-target_names_fruits = ['apple', 'mandarin', 'orange', 'lemon']
+target_names_fruits = fruits["fruit_name"].unique()
 
+X_fruits_2d = fruits[['height', 'width']]
+y_fruits_2d = fruits['fruit_label']
+
+X_train, X_test, y_train, y_test = train_test_split(X_fruits, y_fruits, random_state=0)
+
+from sklearn.preprocessing import MinMaxScaler
+
+scaler = MinMaxScaler()
+# X_train_scaled = scaler.fit_transform(X_train)
+# X_test_scaled = scaler.transform(X_test)
+
+X_train_scaled = X_train
+X_test_scaled = X_test
+
+print(X_train_scaled)
+
+knn = KNeighborsClassifier(n_neighbors = 5)
+knn.fit(X_train_scaled, y_train)
+print('Accuracy of K-NN classifier on training set: {:.2f}'
+     .format(knn.score(X_train_scaled, y_train)))
+print('Accuracy of K-NN classifier on test set: {:.2f}'
+     .format(knn.score(X_test_scaled, y_test)))
+
+
+example_fruit = [[5.5, 2.2, 10, 0.70]]
+print('Predicted fruit type for ', example_fruit, ' is ',
+      target_names_fruits[knn.predict(example_fruit)[0]-1])
