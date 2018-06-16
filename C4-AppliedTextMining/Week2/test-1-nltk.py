59409f21a280adc52ec0e8071dfced63e5492abd
Rahul Patni <rpatni@purdue.edu>
2018-06-16 09:26:36 -0400

    notes on week 2, gonna start assignment

--- C4-AppliedTextMining/Week2/test-1-nltk.py
+++ C4-AppliedTextMining/Week2/test-1-nltk.py
@@ -1,3 +1,32 @@
 import nltk
+from nltk.corpus import treebank
+text17 = treebank.parsed_sents('wsj_0001.mrg')[0]
+print(text17)
 
-import 
+nltk.help.upenn_tagset('MD')
+
+text11 = "Children shouldn't drink a sugary drink before bed."
+
+text13 = nltk.word_tokenize(text11)
+
+text15 = nltk.word_tokenize("Alice loves Bob")
+print(text15)
+
+grammar = nltk.CFG.fromstring("""
+S -> NP VP
+VP -> V NP
+NP -> 'Alice' | 'Bob'
+V -> 'loves'
+""")
+
+print(grammar)
+
+parser = nltk.ChartParser(grammar)
+trees = parser.parse_all(text15)
+for tree in trees:
+    print(tree)
+
+
+print(nltk.help.upenn_tagset())
+
+print(text11.upper())
