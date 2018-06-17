44e3edab7c6653f7ceabc16b8623dcffd0f76864
Rahul Patni <rpatni@purdue.edu>
2018-06-17 10:25:21 -0400

    week 2 stuff done including assignment

--- C4-AppliedTextMining/Week2/assignment.py
+++ C4-AppliedTextMining/Week2/assignment.py
@@ -33,41 +33,74 @@ def answer_one():
 
 
 def answer_three():
-    words = nltk.word_tokenize(moby_raw)
-    dist = FreqDist(words)
+    dist = FreqDist(moby_tokens)
     s = pd.Series(data=dist)
     s = s.sort_values(ascending=False)
     return list(zip(s.index,s))[:20]
 
 
 def answer_four():
-    words = nltk.word_tokenize(moby_raw)
-    words_5 = [word for word in words if len(word) > 5]
+    words_5 = [word for word in moby_tokens if len(word) > 5]
     print(words_5[:20])
     dist = FreqDist(words_5)
-
     print(dist.items())
 
 
 def answer_five():
-    words = nltk.word_tokenize(moby_raw)
-    lengths = [len(word) for word in words]
+    lengths = [len(word) for word in moby_tokens]
     maxindex = np.argmax(lengths)
     return  words[maxindex], lengths[maxindex]
 
 
 def answer_six():
-    words = nltk.word_tokenize(moby_raw)
-    words = [word for word in words if word.isalpha()]
+    words = [word for word in moby_tokens if word.isalpha()]
     dist = FreqDist(words)
     s = pd.Series(data=dist)
     s = s.sort_values(ascending=False)
     s = s.where(s > 2000).dropna()
     return list(zip(s,s.index))[:20]
 
-answer_six()
+
+def answer_seven():
+    sentences = nltk.sent_tokenize(moby_raw)
+    words = list(map(nltk.word_tokenize, sentences))
+    lengths = list(map(len, words))
+
+    return np.average(lengths)
 
 
+def answer_eight():
+    pos = nltk.pos_tag(moby_tokens)
+    dist = FreqDist(np.array(pos)[:, 1])
+    s = pd.Series(data=dist)
+    s = s.sort_values(ascending=False)
+    return list(zip(s.index, s))[:5]
 
-answer_four()
+
+from nltk.corpus import words
+
+correct_spellings = words.words()
+print(correct_spellings)
+
+
+def answer_nine(entries=['cormulent', 'incendenece', 'validrate']):
+    from nltk.metrics.distance import (edit_distance, jaccard_distance)
+    from nltk.util import ngrams
+
+    df = pd.Series(data=correct_spellings)
+    words = [word for word in df if word.startswith(entries[0][0])]
+    res = []
+    for entry in entries:
+        words = [word for word in df if word.startswith(entry[0])]
+        distances = ((jaccard_distance(set(ngrams(entry, 3)),
+                                       set(ngrams(word, 3))), word)
+                     for word in words)
+
+        closest = min(distances)
+
+        res.append(closest[1])
+    return res
+
+
+answer_nine()
 
